f3a49a7aed79f32aea09dbed66906993b05d71e6
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-05 17:27:29 -0500

    Querie to create subjects of "Ingenieria de sistemas"

---
+++ database-queries/subjects.sql
@@ -0,0 +1,59 @@
+INSERT INTO "Subjects"("Name","Classroom","Credits","Slots","Program_Id") VALUES
+	('Algoritmos y Programación','AA_108',4,20, 1),
+	('Cálculo I','AA_312',4,40, 1),
+	('Cátedra Universidad y Entorno','BINF_5',3,40, 1),
+	('Competencias Comunicativas','BINF_8',4,40, 1),
+	('Socio Humanística I','AA_201',3,40, 1),
+	
+	('Álgebra Lineal','BINF_2',3,40, 1),
+	('Cálculo II','AA_318',3,40, 1),
+	('Ética y Política','AA_310',4,40, 1),
+	('Física I','AA_302',4,40, 1),
+	('Programación I','BINF_1',4,20, 1),
+	
+	('Cálculo III','AA_305',3,40, 1),
+	('Economía','AA_316',4,40, 1),
+	('Expresión Gráfica y Geometría Descriptiva','BINF_3',3,20, 1),
+	('Física II','BINF_9',4,40, 1),
+	('Programación II','AA_319',4,20, 1),
+	
+	('Socio Humanística II','AA_309',3,40, 1),
+	('Cálculo IV','AA_308',3,40, 1),
+	('Física III','BINF_4',4,40, 1),
+	('Metodología de la Investigación y Diseño Experimental','AA_304',3,40, 1),
+	('Probabilidad y Estadística','BINF_10',3,40, 1),
+	
+	('Programación III','AA_303',4,20, 1),
+	('Bases de Datos I','BINF_7',4,20, 1),
+	('Electrónica General','AA_306',4,20, 1),
+	('Ingeniería de Requisitos','BINF_6',4,20, 1),
+	('Métodos Numéricos','AA_311',3,40, 1),
+	
+	('Teoría General de Sistemas','AA_320',3,20, 1),
+	('Bases de Datos II','AA_314',4,20, 1),
+	('Comunicaciones','AA_315',4,20, 1),
+	('Ingeniería del Software I','AA_317',4,20, 1),
+	('Investigación de Operaciones','AA_313',4,20, 1),
+	
+	('Matemáticas Discretas','AA_321',3,20, 1),
+	('Ingeniería del Software II','AA_307',4,20, 1),
+	('Lenguajes Formales','AA_301',4,20, 1),
+	('Sistemas Distribuidos','AA_308',3,20, 1),
+	('Sistemas Operativos','BINF_1',4,20, 1),
+	
+	('Transmisión de Datos','BINF_2',4,20, 1),
+	('Administración','AA_309',3,40, 1),
+	('Arquitectura de Computadores','AA_310',3,20, 1),
+	('Inteligencia Computacional','BINF_3',4,20, 1),
+	('Redes de Datos','AA_311',4,20, 1),
+	
+	('Trabajo de Campo','BINF_4',4,20, 1),
+	('Auditoría de Sistemas','AA_312',3,20, 1),
+	('Electiva I','AA_313',3,20, 1),
+	('Electiva II','AA_314',3,20, 1),
+	('Gerencia Informática','BINF_5',3,40, 1),
+	
+	('Seminario de Trabajo de Grado','AA_315',3,20, 1),
+	('Simulación por Computador','AA_316',4,20, 1),
+	('Electiva III','BINF_6',3,20, 1),
+	('Electiva IV','AA_317',3,20, 1);
