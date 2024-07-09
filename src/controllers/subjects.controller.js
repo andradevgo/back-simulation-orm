2319eb8425087b3a9e183b9f8ad93fbef6baa13b
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-09 14:44:59 -0500

    Add create subjects functionality

--- src/controllers/subjects.controller.js
+++ src/controllers/subjects.controller.js
@@ -22,3 +22,50 @@ export const getSubject = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const createSubject = async (req, res) => {
+  const { Name, Clasroom, Credits, Slots, Program_Id } = req.body;
+  try {
+    const newSubject = await prisma.subjects.create({
+      data: {
+        Name,
+        Clasroom,
+        Credits: +Credits,
+        Slots: +Slots,
+        Program_Id: +Program_Id,
+      },
+    });
+    res.json(newSubject);
+  } catch (error) {}
+};
+
+export const getStudentsBySubject = async (req, res) => {
+  const { id, division } = req.params;
+  console.log(id);
+  try {
+    const students = await prisma.students.findMany({
+      where: {
+        Enrollments: {
+          some: {
+            Subject_Id: +id,
+            Subject_group: +division,
+          },
+        },
+      },
+    });
+    const studentsCount = await prisma.students.count({
+      where: {
+        Enrollments: {
+          some: {
+            Subject_Id: +id,
+            Subject_group: +division,
+          },
+        },
+      },
+    });
+    res.json(students);
+    console.log(studentsCount);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
