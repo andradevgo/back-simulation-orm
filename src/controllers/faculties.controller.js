57c9e97e5a812135b5d3f759e42c385186e8b1bd
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-09 11:13:38 -0500

    Add functionality create faculty

--- src/controllers/faculties.controller.js
+++ src/controllers/faculties.controller.js
@@ -4,3 +4,13 @@ export const getFaculties = async (req, res) => {
   const faculties = await prisma.faculties.findMany();
   res.json(faculties);
 };
+
+export const createFaculty = async (req, res) => {
+  const { Name } = req.body;
+  const newFaculty = await prisma.faculties.create({
+    data: {
+      Name,
+    },
+  });
+  res.json(newFaculty);
+};
