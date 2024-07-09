fafffe4115376c3e66582f3f75cc8766d11896e4
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-09 14:45:30 -0500

    Add enrollments routes

--- src/routes/enrollments.routes.js
+++ src/routes/enrollments.routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
 import {
+  createEnrollment,
+  deleteEnrollment,
   getEnrollment,
   getEnrollments,
+  updateEnrollment,
 } from '../controllers/enrollments.controller.js';
 
 const router = Router();
 
 router.get('/enrollments', getEnrollments);
 router.get('/enrollments/:id', getEnrollment);
+router.post('/enrollments', createEnrollment);
+router.put('/enrollments/:id', updateEnrollment);
+router.delete('/enrollments/:id', deleteEnrollment);
 
 export default router;
