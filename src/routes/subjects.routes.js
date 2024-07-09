c42ef71030677ca76c7564d1c4345164591ba467
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-09 14:46:43 -0500

    Add subjects routes

--- src/routes/subjects.routes.js
+++ src/routes/subjects.routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
-import { getSubject, getSubjects } from '../controllers/subjects.controller.js';
+import {
+  createSubject,
+  getStudentsBySubject,
+  getSubject,
+  getSubjects,
+} from '../controllers/subjects.controller.js';
 
 const router = Router();
 
 router.get('/subjects', getSubjects);
 router.get('/subjects/:id', getSubject);
+router.get('/subjects/:id/:division/students', getStudentsBySubject);
+router.post('/subjects', createSubject);
 
 export default router;
