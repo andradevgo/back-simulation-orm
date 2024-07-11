b2da11dbbf0d1e66958f92fbc0cbc834b369f180
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-10 20:17:33 -0500

    Add route getStudentSubjects

--- src/routes/students.routes.js
+++ src/routes/students.routes.js
@@ -3,13 +3,14 @@ import {
   createStudent,
   getStudent,
   getStudents,
+  getStudentSubjects,
 } from '../controllers/student.controller.js';
 
 const router = Router();
 
 router.get('/students', getStudents);
 router.get('/students/:id', getStudent);
-
+router.get('/students/:id/subjects', getStudentSubjects);
 router.post('/students', createStudent);
 
 export default router;
