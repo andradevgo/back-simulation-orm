3134ee1543f1a92f51309708b565450fea2d8fbe
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-12 15:58:40 -0500

    feat: Add routes for users and roles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import programsRoutes from './routes/programs.routes.js';
 import studentsRoutes from './routes/students.routes.js';
 import subjectsRoutes from './routes/subjects.routes.js';
 import facultiesRoutes from './routes/faculties.routes.js';
+import usersRoutes from './routes/users.routes.js';
+import rolesRoutes from './routes/roles.routes.js';
 import cors from 'cors';
 
 const app = express();
@@ -11,11 +13,15 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
 app.use('/api', facultiesRoutes);
 app.use('/api', programsRoutes);
 app.use('/api', studentsRoutes);
 app.use('/api', subjectsRoutes);
 app.use('/api', enrollmentsRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api', enrollmentsRoutes);
+app.use('/api', rolesRoutes);
 
 app.listen(3000, () => {
   console.log('Server on port', 3000);
