ae1e0b06edb01b05df5542c288bfa560c4a231d6
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-07-22 23:26:36 -0500

    Update server port configuration and start script

--- src/index.js
+++ src/index.js
@@ -36,5 +36,5 @@ app.use('/api', enrollmentsRoutes);
 app.use('/api', rolesRoutes);
 
 app.listen(PORT, () => {
-  console.log('Server on port', 3000);
+  console.log(`Server on port ${PORT}`);
 });
